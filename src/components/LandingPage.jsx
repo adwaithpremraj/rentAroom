10d9dffe41b311a74f70cff89c14a4504ea8dc43
akshaykrishnap <akshaykrishnap071299@gmail.com>
2024-04-25 17:08:17 +0530

    landingpage b& city cards

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Landing.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons';
+
+
+function Landing_Page() {
+
+
+
+    return (
+        <>
+            <div className='landing '>
+
+
+                <h2 className='heading' style={{ color: "white" }}>Find Hotels and PGs Which Suits Your Needs</h2>
+
+                <h4 className='heading' style={{ color: "white" }}>Browse Our Website For Your Needs</h4>
+                <br />
+                <form >
+                    <input id='forms' style={{ width: '450px' }} className='form-control align-items-center' type="text" placeholder='Search: Cities,State...' /><br />
+                    <select id='forms1' className="form-select " >
+                        <option selected>Select Gender</option>
+                        <option value="">Men</option>
+                        <option value="">Women</option>
+                        <option value="">Others</option>
+                    </select>
+                    <button id='explore' className='btn'>Explore More <FontAwesomeIcon icon={faArrowRight} /></button>
+                </form>
+
+            </div>
+        </>
+    )
+}
+
+export default Landing_Page
